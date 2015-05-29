50b3ef617534ece37142ce2f31e5fca4b62db414
Roger JangKun Kim <rb2stk@gmail.com>
2015-05-29 10:42:12 -0700

    yay

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" id="font-awesome-css" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" type="text/css" media="screen">
+</head>
+<body>
+	<nav class="projectlink1">
+		<ul class="projectlink2">
+			<li class="projectlink3">
+				<a class="projectlink4" href="#screen2"><strong>Project 1</strong></a>	
+			</li>
+			<li class="projectlink3">
+				<a class="projectlink4" href="#screen3"><strong>Project 2</strong></a>	
+			</li>
+			<li class="projectlink3">
+				<a class="projectlink4" href="#screen4"><strong>Project 3</strong></a>	
+			</li>
+		</ul>
+	</nav>
+	<div id="opacity">
+		<div id="header">
+		</div>
+	</div>
+	<div id="screen2">
+		<div id="body1">
+			
+		</div>
+		<div id="body15">
+		<a class="weblink" href="../AwesomenautsKnockOff/index.php">WHOO</a>
+		</div>
+	</div>
+	<div id="screen3">
+		<div id="body2">
+			
+		</div>
+		<div id="body25">
+			<a class="weblink" href="../appapi/index.php">WHOO</a>
+		</div>
+	</div>
+	<div id="screen4">
+		<div id="body3">
+			
+		</div>
+		<div id="body35">
+			<a class="weblink" href="../blog/index.php">WHOO</a>
+		</div>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script type="text/javascript" src="http://arrow.scrolltotop.com/arrow59.js"></script>
+</body>
+</html>
